15976a2bcb6f431698d1b6b165920fa8aa73eb5d
YasiYi <yasi.yousefi96@gmail.com>
2025-07-25 21:19:50 +0330

    init first http request

--- academy/academy/settings.py
+++ academy/academy/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'club.apps.ClubConfig'
+    'club.apps.ClubConfig',
 ]
 
 MIDDLEWARE = [
